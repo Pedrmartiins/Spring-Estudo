59febbb2b4b7e1fc19701e16d6f73f2e85acab75
pedro <pedroenriquellopes2011@gmail.com>
2025-09-05 22:38:57 -0300

    To do list

--- AULA3/TO-DO-LIST/src/main/java/br/com/aweb/to_do_list/model/Todo.java
+++ AULA3/TO-DO-LIST/src/main/java/br/com/aweb/to_do_list/model/Todo.java
@@ -3,11 +3,17 @@ package br.com.aweb.to_do_list.model;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -29,13 +35,17 @@ public class Todo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
-   
+   @Size(min = 3, max = 100)
+   @NotBlank
     @Column(length = 100, nullable = false)
 
     private String title;
 
     @Column(nullable = false)
     private LocalDateTime createdAt = LocalDateTime.now();
+
+    @FutureOrPresent
+    @DateTimeFormat(iso = ISO.DATE)
     @Column(nullable = false)
     private LocalDate deadLine;
     @Column(nullable = true)
