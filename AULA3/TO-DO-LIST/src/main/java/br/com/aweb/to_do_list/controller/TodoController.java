32e36935f4039eec3758dc4e1347b4f828dfd5d7
pedro <pedroenriquellopes2011@gmail.com>
2025-09-05 21:03:25 -0300

    To do list

--- AULA3/TO-DO-LIST/src/main/java/br/com/aweb/to_do_list/controller/TodoController.java
+++ AULA3/TO-DO-LIST/src/main/java/br/com/aweb/to_do_list/controller/TodoController.java
@@ -1,26 +1,38 @@
 package br.com.aweb.to_do_list.controller;
 
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.aweb.to_do_list.repository.TodoRepository;
+
 @Controller
 @RequestMapping("/todo")
 public class TodoController {
-    @GetMapping("/home")
-    public ModelAndView home(){
-        var modelAndView = new ModelAndView("home");
-        modelAndView.addObject("professor","Andre");
-        var alunos = List.of(
-            "Isaac",
-            "albert",
-            "maria"
-        );
-        modelAndView.addObject("alunos", alunos);
-        modelAndView.addObject("ehVerdade", false);
+
+    @Autowired
+    TodoRepository todoRepository;
+    
+
+    // @GetMapping("/home")
+    // public ModelAndView home(){
+    //     var modelAndView = new ModelAndView("home");
+    //     modelAndView.addObject("professor","Andre");
+    //     var alunos = List.of(
+    //         "Isaac",
+    //         "albert",
+    //         "maria"
+    //     );
+    //     modelAndView.addObject("alunos", alunos);
+    //     modelAndView.addObject("ehVerdade", false);
+    //     return modelAndView;
+    // }
+    @GetMapping
+    public ModelAndView list(){
+        ModelAndView modelAndView = new ModelAndView("list");
+        modelAndView.addObject("todos", todoRepository.findAll());
         return modelAndView;
     }
 }
--- AULA3/TO-DO-LIST/src/main/java/br/com/aweb/to_do_list/model/Todo.java
+++ AULA3/TO-DO-LIST/src/main/java/br/com/aweb/to_do_list/model/Todo.java
@@ -8,15 +8,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@ToString
 
 public class Todo {
 
